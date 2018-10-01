5770ebf9c72aa94c05ab48fc7d6a712f5c1619bd
rodrigoTopan <rodrigo.topan.ti@gmail.com>
2018-09-30 22:34:43 -0300

    Fix/Web Routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,10 +9,28 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+	return view('index');
 });
+#Controllers Routes - Index, Create, Store, Show, Edit, Update & Destroy
+Route::resource('activities', 'ActivityController');
+Route::resource('certificates', 'CertificateController');
+Route::resource('dashboards', 'DashboardController');
+Route::resource('events', 'EventController');
+Route::resource('feedbacks', 'FeedbackQuizController');
+Route::resource('feeds', 'FeedController');
+Route::resource('locations', 'LocationController');
+Route::resource('quizzes', 'QuizController');
+Route::resource('rooms', 'RoomController');
+Route::resource('schedules', 'ScheduleController');
+Route::resource('speakers', 'SpeakerController');
+Route::resource('tickets', 'TicketController');
+Route::resource('infos', 'InternalInfoController');
+Route::resource('subscriptions', 'SubscriptionController');
+
+#Controller Routes - Differents Kinds of routes
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
 
 
 Route::get('user/verify/{verification_code}', 'AuthController@verifyUser');
